75144c7d1d26958b89444b604615aebde639a6d0
asaf bello <asafbello@gmail.com>
2017-11-07 18:11:05 +0200

    change links

--- js/portfolioLoader.js
+++ js/portfolioLoader.js
@@ -1,11 +1,11 @@
 
 var gProjects = [
-    { id: '0', name: 'MineSweeper', title: 'Do not die', desc: 'Click all non-bombs cells to win', lables: ['matrixes', 'mouse clicks'], url: 'img/games/minesweeper.jpg', link: '../projs/minesweeper/index.html' },
-    { id: '1', name: 'Sokoban', title: 'Lets push', desc: 'Put all boxes on targets to win', lables: ['matrixes', 'mouse clicks'], url: 'img/games/sokoban.jpg', link: '../projs/sokoban/sokoban.html' },
-    { id: '2', name: 'chess', title: 'chess', desc: 'Kill the opposing king before he kills you', lables: ['matrixes', 'mouse clicks'], url: 'img/games/chess.jpg', link: '../projs/chess/index.html' },
-    { id: '3', name: 'calculator', title: 'just calac', desc: 'check how smart you are', lables: ['calcs', 'mouse clicks'], url: 'img/games/calc.png', link: '../projs/calcu/calcu.html' },
-    { id: '4', name: 'Pop ballons', title: 'click fast', desc: 'pop all ballons as fast as you can', lables: ['ballons', 'mouse clicks'], url: 'img/games/pop.jpg', link: '../projs/balloon-pop/index.html' },
-    { id: '5', name: 'In picture', title: 'guess!', desc: 'guess the right description to win', lables: ['mouse clicks'], url: 'img/games/inpic.jpg', link: '../projs/in-picture/index.html' },
+    { id: '0', name: 'MineSweeper', title: 'Do not die', desc: 'Click all non-bombs cells to win', lables: ['matrixes', 'mouse clicks'], url: 'img/games/minesweeper.jpg', link: 'projs/minesweeper/index.html' },
+    { id: '1', name: 'Sokoban', title: 'Lets push', desc: 'Put all boxes on targets to win', lables: ['matrixes', 'mouse clicks'], url: 'img/games/sokoban.jpg', link: 'projs/sokoban/sokoban.html' },
+    { id: '2', name: 'chess', title: 'chess', desc: 'Kill the opposing king before he kills you', lables: ['matrixes', 'mouse clicks'], url: 'img/games/chess.jpg', link: 'projs/chess/index.html' },
+    { id: '3', name: 'calculator', title: 'just calac', desc: 'check how smart you are', lables: ['calcs', 'mouse clicks'], url: 'img/games/calc.png', link: 'projs/calcu/calcu.html' },
+    { id: '4', name: 'Pop ballons', title: 'click fast', desc: 'pop all ballons as fast as you can', lables: ['ballons', 'mouse clicks'], url: 'img/games/pop.jpg', link: 'projs/balloon-pop/index.html' },
+    { id: '5', name: 'In picture', title: 'guess!', desc: 'guess the right description to win', lables: ['mouse clicks'], url: 'img/games/inpic.jpg', link: 'projs/in-picture/index.html' },
 ];
 
 function addProjectsDiv(proj) {
